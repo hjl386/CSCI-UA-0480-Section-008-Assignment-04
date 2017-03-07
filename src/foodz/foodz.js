6a429b75f5aacd67518a1468d9ee2503703e5d57
Han Jie Lee <hjl386@nyu.edu>
2017-03-07 10:42:58 -0500

    Implementing filters

--- src/foodz/foodz.js
+++ src/foodz/foodz.js
@@ -1,1 +1,25 @@
 // foodz.js
+
+const express = require('express');
+const app = express();
+const [PORT, HOST] = [8080, '127.0.0.1'];
+
+app.use(express.static('public'));
+app.set('view engine', 'hbs');
+
+let dataGlobal = [{name:'chocoramen', description:'ramen noodles in a chocolate almond milk broth', category:'breakfast'}, {name:'lycheezy', description:'cheese pizza with lychee on top', category:'anytime'}, {name:'crazy cookie', description:'a 1 foot diameter cookie', category:'dinner'}];
+
+app.get('/css/base.css', (req, res) => {
+	res.render('base.css');
+});
+
+app.get('/', (req, res) => {
+	let data = dataGlobal.filter((ele, index) => {
+		if(req.query.filterCategory === ele.category){
+			return ele;
+		}
+	});
+	res.render('foodz', {'title':'Foodz', 'database':data, 'dataG':dataGlobal});
+});
+
+app.listen(PORT, HOST);
